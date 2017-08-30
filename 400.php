2133f3dec063555e29e26a07bdee0da656e26b69
vExam <31455147+vexam@users.noreply.github.com>
2017-08-30 18:36:00 +0300

    .

--- 400.php
+++ 400.php
@@ -8,7 +8,6 @@ Contact: support@keenthemes.com
 Follow: www.twitter.com/keenthemes
 Dribbble: www.dribbble.com/keenthemes
 Like: www.facebook.com/keenthemes
-Purchase: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
 Renew Support: http://themeforest.net/item/metronic-responsive-admin-dashboard-template/4021469?ref=keenthemes
 License: You must have a valid license purchased only from themeforest(the above link) in order to legally use the theme for your project.
 -->
